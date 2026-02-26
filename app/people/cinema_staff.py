b53e5a83825d5008f9006b21bfae76682ce9a9ae
fanghey <2603Lysenko2603@gmail.com>
2026-02-26 19:59:42 +0100

    Update cinema_staff.py

--- app/people/cinema_staff.py
+++ app/people/cinema_staff.py
@@ -1,1 +1,7 @@
+class cleaner
+    def __init__(self, name):
+        self.name = name
+
+    def clean_hall(self, hall_number):
+        print(f"{self.name} is cleaning half_number {hall_number}")
 
